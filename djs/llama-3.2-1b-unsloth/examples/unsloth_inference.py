d5838048795952a559e984458d5953db248f6125
Daniel Smedema <djsmedes@gmail.com>
2024-11-17 23:25:20 +0000

    train and inference

--- djs/llama-3.2-1b-unsloth/examples/unsloth_inference.py
+++ djs/llama-3.2-1b-unsloth/examples/unsloth_inference.py
@@ -5,7 +5,9 @@ from trl import SFTTrainer
 from transformers import TrainingArguments
 from transformers import TextStreamer
 from datasets import load_dataset
+import os
 
+SAVED_WEIGHTS_DIR = "saved_weights"
 max_seq_length = 2048  # Supports RoPE Scaling interally, so choose any!
 
 # 4bit pre quantized models we support for 4x faster downloading + no OOMs.
@@ -16,12 +18,6 @@ fourbit_models = [
     "unsloth/Llama-3.2-3B-Instruct-bnb-4bit",
 ]  # More models at https://huggingface.co/unsloth
 
-model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name="unsloth/Llama-3.2-3B-Instruct-bnb-4bit",
-    max_seq_length=max_seq_length,
-    dtype=None,
-    load_in_4bit=True,
-)
 
 alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
@@ -34,35 +30,59 @@ alpaca_prompt = """Below is an instruction that describes a task, paired with an
 ### Response:
 {}"""
 
-FastLanguageModel.for_inference(model)  # Enable native 2x faster inference
-inputs = tokenizer(
-    [
-        alpaca_prompt.format(
-            "Continue the fibonnaci sequence.",  # instruction
-            "1, 1, 2, 3, 5, 8",  # input
-            "",  # output - leave this blank for generation!
-        )
-    ],
-    return_tensors="pt",
-).to("cuda")
 
-print()
-print(f"Input tokens: {len(inputs[0])}")
-print()
+def inference(*, local_model_name=None):
+    model_name = (
+        os.path.join(SAVED_WEIGHTS_DIR, local_model_name)
+        if local_model_name is not None
+        else "unsloth/Llama-3.2-3B-Instruct-bnb-4bit"
+    )
 
-text_streamer = TextStreamer(tokenizer)
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name=model_name,
+        max_seq_length=max_seq_length,
+        dtype=None,
+        load_in_4bit=True,
+    )
 
-outputs = model.generate(
-    **inputs,
-    streamer=text_streamer,
-    max_new_tokens=128,
-    # use_cache=True
-)
+    print(dir(model))
 
-(decoded_outputs,) = tokenizer.batch_decode(outputs)
+    return
 
-print()
+    FastLanguageModel.for_inference(model)  # Enable native 2x faster inference
+    inputs = tokenizer(
+        [
+            alpaca_prompt.format(
+                "Give the next number of the fibonnaci sequence.",  # instruction
+                "1, 1, 2, 3, 5, 8",  # input
+                "",  # output - leave this blank for generation!
+            )
+        ],
+        return_tensors="pt",
+    ).to("cuda")
 
-print(f"Tokens output: {len(outputs[0])}")
-print(f"Final token: {tokenizer.decode(outputs[0][-1])}")
-# print(decoded_outputs)
+    n_input_tokens = len(inputs[0])
+
+    print()
+
+    text_streamer = TextStreamer(tokenizer)
+
+    outputs = model.generate(
+        **inputs,
+        streamer=text_streamer,
+        max_new_tokens=256,
+        # use_cache=True
+    )
+
+    (decoded_outputs,) = tokenizer.batch_decode(outputs)
+
+    print()
+
+    print(f"Tokens output: {len(outputs[0]) - n_input_tokens}")
+    print(f"Complete? {outputs[0][-1] == tokenizer.eos_token_id}")
+
+    # print(decoded_outputs)
+
+
+if __name__ == "__main__":
+    inference(local_model_name="stub")
---
+++ djs/llama-3.2-1b-unsloth/examples/unsloth_train.py
@@ -0,0 +1,91 @@
+from unsloth import FastLanguageModel
+from unsloth import is_bfloat16_supported
+import torch
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from datasets import load_dataset
+import os
+
+SAVED_WEIGHTS_DIR = "saved_weights"
+max_seq_length = 2048  # Supports RoPE Scaling interally, so choose any!
+
+# 4bit pre quantized models we support for 4x faster downloading + no OOMs.
+fourbit_models = [
+    "unsloth/Llama-3.2-1B-bnb-4bit",  # NEW! Llama 3.2 models
+    "unsloth/Llama-3.2-1B-Instruct-bnb-4bit",
+    "unsloth/Llama-3.2-3B-bnb-4bit",
+    "unsloth/Llama-3.2-3B-Instruct-bnb-4bit",
+]  # More models at https://huggingface.co/unsloth
+
+
+def get_relative_path(filename):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(dir_path, filename)
+
+
+def lora_finetune(*, training_data_filename, model_output_dir):
+
+    dataset = load_dataset(
+        "json", data_files=get_relative_path(training_data_filename), split="train"
+    )
+
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name="unsloth/Llama-3.2-3B-Instruct-bnb-4bit",
+        max_seq_length=max_seq_length,
+        dtype=None,
+        load_in_4bit=True,
+    )
+
+    # Do model patching and add fast LoRA weights
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r=16,
+        target_modules=[
+            "q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj",
+            "gate_proj",
+            "up_proj",
+            "down_proj",
+        ],
+        lora_alpha=16,
+        lora_dropout=0,  # Supports any, but = 0 is optimized
+        bias="none",  # Supports any, but = "none" is optimized
+        # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+        use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+        random_state=3407,
+        max_seq_length=max_seq_length,
+        use_rslora=False,  # We support rank stabilized LoRA
+        loftq_config=None,  # And LoftQ
+    )
+
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=dataset,
+        dataset_text_field="text",
+        max_seq_length=max_seq_length,
+        tokenizer=tokenizer,
+        args=TrainingArguments(
+            per_device_train_batch_size=2,
+            gradient_accumulation_steps=4,
+            warmup_steps=10,
+            max_steps=1,  # 60,
+            fp16=not is_bfloat16_supported(),
+            bf16=is_bfloat16_supported(),
+            logging_steps=1,
+            output_dir="outputs",
+            optim="adamw_8bit",
+            seed=3407,
+        ),
+    )
+
+    trainer.train()
+
+    save_to_dir = os.path.join(SAVED_WEIGHTS_DIR, model_output_dir)
+    model.save_pretrained(save_to_dir)
+    tokenizer.save_pretrained(save_to_dir)
+
+
+if __name__ == "__main__":
+    lora_finetune(training_data_filename="stub.jsonl", model_output_dir="stub")
