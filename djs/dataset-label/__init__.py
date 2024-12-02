b59c795778f1896e435920fb49c3b6bd2b07f855
Daniel Smedema <djsmedes@gmail.com>
2024-12-02 02:28:20 +0000

    add ids

--- djs/dataset-label/__init__.py
+++ djs/dataset-label/__init__.py
@@ -1,9 +1,8 @@
 import os
-from pprint import pprint
 import random
 import json
 
-DIFF_DATA_ROOT = "/shared/project/mondo/bugswarm-data/data/task_diff"
+DATA_ROOT = "/shared/project/mondo/bugswarm-data/data/filtered_job_logs"
 FAILED = "failed.log"
 PASSED = "passed.log"
 CONTEXT_WINDOW = 4
@@ -22,38 +21,43 @@ def generate_subdirs_list(*, parent_dir):
     return abs_paths
 
 
+def generate_logfiles_list(*, parent_dir):
+    sorted_filenames = sorted(
+        file for _root, _dirs, files in os.walk(parent_dir) for file in files
+    )
+    abs_paths = [os.path.join(parent_dir, filename) for filename in sorted_filenames]
+    return abs_paths
+
+
 def process(*, abs_paths, output_abs_path):
 
     with open(output_abs_path, "a") as out_file:
         # open file from random choices
         for input_abs_path in abs_paths:
             with open(input_abs_path, "r") as in_file:
-                # choose random lines
                 lines = in_file.readlines()
-                start_line = random.randint(0, len(lines) - CONTEXT_WINDOW)
-                chosen_lines = lines[start_line : start_line + CONTEXT_WINDOW]
-                print("".join(chosen_lines))
-                y_or_n = input("Lines contain error? (y/N) ")
-                result = False
-                if y_or_n in ["y", "Y"]:
-                    result = True
-                elif y_or_n in ["", "n", "N"]:
-                    pass
-                else:
-                    raise ValueError(f"Unrecognized input")
-
-    # read random lines (into new file?)
-    # print random lines
-    # use python "input" to ask human whether error is contained
-    # write to jsonl file with the text and the classification
-
-    # problem: extremely unbalanced data. likely never find the error at all
-
-    pass
+            # choose random lines
+            start_line = random.randint(0, len(lines) - CONTEXT_WINDOW)
+            chosen_lines = "".join(lines[start_line : start_line + CONTEXT_WINDOW])
+            print()
+            print(chosen_lines)
+            y_or_n = input("Lines contain error? (y/N) ")
+            result = False
+            if y_or_n in ["y", "Y"]:
+                result = True
+            elif y_or_n in ["", "n", "N"]:
+                pass
+            else:
+                raise ValueError(f"Unrecognized input")
+            out_file.write(
+                json.dumps(
+                    {"id": input_abs_path, "text": chosen_lines, "label": result}
+                )
+            )
 
 
 if __name__ == "__main__":
-    subdirs = generate_subdirs_list(parent_dir=DIFF_DATA_ROOT)
+    subdirs = generate_logfiles_list(parent_dir=DATA_ROOT)
     random.seed(6745)
     chosen = random.choices(subdirs, k=10)
     process(abs_paths=chosen, output_abs_path=get_relative_path("output.jsonl"))
